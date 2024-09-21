14731c4a7c50871db4f5a8977f2f6379bb6fb6e5
Pranav Yamagekar <pranav.yamagekar@gmail.com>
2024-09-21 17:40:10 +0530

    Revert "DBMS_Update"
    This reverts commit 7471f4d30d4cdb083661527f73ca8ff2f46eaaea.

--- Backend/config/db.sql
+++ Backend/config/db.sql
@@ -35,49 +35,18 @@ CREATE TABLE auth (
 | phone          | bigint unsigned | NO   |     | NULL    |                |
 +----------------+-----------------+------+-----+---------+----------------+
 
--- Create table patient_table
-CREATE TABLE patient_personal_info (
-  patient_id bigint(15) PRIMARY KEY AUTO_INCREMENT,
-  patient_fname varchar(45) NOT NULL,
-  patient_lname varchar(45) NOT NULL,
-  patient_mname varchar(25) NOT NULL,
-  gender int(11) NOT NULL,
-  birthday date NOT NULL,
-  address1 text NOT NULL,
-  address2 text NOT NULL,
-  age int(5) NOT NULL,
-  mobile_no_1 varchar(25) NOT NULL,
-  mobile_no_2 varchar(25) NOT NULL,
-  email_address varchar(50) NOT NULL,
-  picture varchar(100) NOT NULL,
-  date_entry datetime NOT NULL,
-  blood_group int(11) NOT NULL,
-  Insurance_comp int(11) NOT NULL,
-  insurance_no varchar(25) NOT NULL,
-  id_identifiers text NOT NULL,
-  InActive int(11) NOT NULL
-);
-
-+----------------+--------------+------+-----+---------+----------------+
-| Field          | Type         | Null | Key | Default | Extra          |
-+----------------+--------------+------+-----+---------+----------------+
-| patient_id     | bigint       | NO   | PRI | NULL    | auto_increment |
-| patient_fname  | varchar(45)  | NO   |     | NULL    |                |
-| patient_lname  | varchar(45)  | NO   |     | NULL    |                |
-| patient_mname  | varchar(25)  | NO   |     | NULL    |                |
-| gender         | int          | NO   |     | NULL    |                |
-| birthday       | date         | NO   |     | NULL    |                |
-| address1       | text         | NO   |     | NULL    |                |
-| address2       | text         | NO   |     | NULL    |                |
-| age            | int          | NO   |     | NULL    |                |
-| mobile_no_1    | varchar(25)  | NO   |     | NULL    |                |
-| mobile_no_2    | varchar(25)  | NO   |     | NULL    |                |
-| email_address  | varchar(50)  | NO   |     | NULL    |                |
-| picture        | varchar(100) | NO   |     | NULL    |                |
-| date_entry     | datetime     | NO   |     | NULL    |                |
-| blood_group    | int          | NO   |     | NULL    |                |
-| Insurance_comp | int          | NO   |     | NULL    |                |
-| insurance_no   | varchar(25)  | NO   |     | NULL    |                |
-| id_identifiers | text         | NO   |     | NULL    |                |
-| InActive       | int          | NO   |     | NULL    |                |
-+----------------+--------------+------+-----+---------+----------------+
+-- Create table patient
+CREATE TABLE ⁠ patient ⁠ (
+  ⁠ patient_id ⁠ varchar(50) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  ⁠ patient_fname ⁠ varchar(45) NOT NULL,
+  ⁠ patient_lname ⁠ varchar(45) NOT NULL,
+  ⁠ blood_type ⁠ varchar(10) DEFAULT NULL,
+  ⁠ email ⁠ varchar(45) DEFAULT NULL,
+  ⁠ gender ⁠ varchar(45) NOT NULL,
+  ⁠ age ⁠ int(5) NOT NULL,
+  ⁠ admission_date ⁠ date DEFAULT NULL,
+  ⁠ discharge_date ⁠ varchar(45) DEFAULT NULL,
+  ⁠ phone ⁠ bigint unsigned NOT NULL,
+  PRIMARY KEY (⁠ patient_id ⁠),
+  UNIQUE KEY ⁠ patient_id_UNIQUE ⁠ (⁠ patient_id ⁠)
+) 
